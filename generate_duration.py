71d30cbbec44cb337cbfa59263858e9f08fa686b
liangshuang1993 <liangshuang1993@126.com>
2019-06-05 16:44:49 +0800

    fix

--- generate_duration.py
+++ generate_duration.py
@@ -3,12 +3,50 @@ import numpy as np
 import time
 import torch
 import argparse
+import importlib
 
-from train import setup_loader
 from utils.text import phonemes, symbols
 from utils.generic_utils import load_config
+from utils.audio import AudioProcessor
+from datasets.TTSDataset import MyDataset
+from distribute import DistributedSampler
 from models.tacotron import Tacotron
 
+
+def setup_loader(c, is_val=False, verbose=False):
+    global ap
+    if is_val and not c.run_eval:
+        loader = None
+    else:
+        preprocessor = importlib.import_module('datasets.preprocess')
+        preprocessor = getattr(preprocessor, c.dataset.lower())
+        dataset = MyDataset(
+            c.data_path,
+            c.meta_file_val if is_val else c.meta_file_train,
+            c.r,
+            c.text_cleaner,
+            preprocessor=preprocessor,
+            ap=ap,
+            batch_group_size=0 if is_val else c.batch_group_size * c.batch_size,
+            min_seq_len=0 if is_val else c.min_seq_len,
+            max_seq_len=float("inf") if is_val else c.max_seq_len,
+            cached=False if c.dataset != "tts_cache" else True,
+            phoneme_cache_path=c.phoneme_cache_path,
+            use_phonemes=c.use_phonemes,
+            phoneme_language=c.phoneme_language,
+            verbose=verbose)
+        sampler = DistributedSampler(dataset) if num_gpus > 1 else None
+        loader = DataLoader(
+            dataset,
+            batch_size=c.eval_batch_size if is_val else c.batch_size,
+            shuffle=False,
+            collate_fn=dataset.collate_fn,
+            drop_last=False,
+            sampler=sampler,
+            num_workers=c.num_val_loader_workers
+            if is_val else c.num_loader_workers,
+            pin_memory=False)
+    return loader
 
 def get_duration(alignment):
     t, s = alignment.shape # t is phoneme's length, s is spectrogram's length
@@ -29,6 +67,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='synthesis parameters')
     parser.add_argument('--root', help='Please input root path', required=True)
     parser.add_argument('--step', help='Please input step', required=False)
+    args = parser.parse_args()
 
     ROOT_PATH = args.root
     MODEL_PATH_TMP = ROOT_PATH + '/checkpoint_{}.pth.tar'
